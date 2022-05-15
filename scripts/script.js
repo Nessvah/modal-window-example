df6d1832a521c425831777b51128a0e6271fd015
Nessvah <vanessamarquesns@outlook.pt>
2022-05-15 02:08:27 +0100

    fix bug where it wasnt adding event listeners to all elements of node list

--- scripts/script.js
+++ scripts/script.js
@@ -7,23 +7,30 @@ const btnCloseModal = document.querySelector(".btn__close-modal");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 
+// Create some functions
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
 // Add Event Listeners to every button
 
-for (i = 0; i < btnsOpenModal.length; i++) {
+for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener("click", openModal);
-}
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
-// Create some functions
+// Dealing with esc keypress to close modal
 
-const openModal = function () {
-  modal.classList.remove(".hidden");
-  overlay.classList.remove(".hidden");
-};
-
-const closeModal = function () {
-  modal.classList.add(".hidden");
-  overlay.classList.add(".hidden");
-};
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
